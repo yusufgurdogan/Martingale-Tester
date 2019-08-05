762d4dc73e1ea32d077aa7417557531a6bea2a2a
Yusuf Gürdoğan <13736056+yusufgurdogan@users.noreply.github.com>
2019-08-05 21:21:16 +0300

    Random number generator
    - Generates a random integer between 0 - 100. If the number is less than 50, you win. Otherwise, you lose.

--- mips.asm
+++ mips.asm
@@ -4,6 +4,10 @@ enterhouseedge: .asciiz "Enter house edge (%):"
 enterbetsize: .asciiz "Enter initial bet size:"
 entergoal: .asciiz "Enter your goal (for doubling your money, enter 2):"
 BetSizeGreater: .asciiz "Bet size can not be greater than your starting balance!\n"
+Win: .asciiz "WIN!\n"
+Lose: .asciiz "LOSE!\n"
+betsize: .asciiz "bet size: "
+balance: .asciiz "balance: "
 .text
 .globl main
 main:
@@ -32,7 +36,7 @@ li $v0, 5 # scanf bet size
 syscall 
 move $a2, $v0
 
-bgt $a2, $a1, IfBetSizeGreater # If bet size is greater, jump to IfBetSizeGreater
+bgt $a2, $a0, IfBetSizeGreater # If bet size is greater, jump to IfBetSizeGreater
 
 la $a0, entergoal
 li $v0, 4 # "Enter your goal (for doubling your money, enter 2):"
@@ -41,6 +45,26 @@ syscall
 li $v0, 5 # scanf goal
 syscall 
 move $a3, $v0
+
+j loop
+loop:
+li $a1, 100 # random number interval is 0-100, inclusive
+li $v0, 42 # generate random number
+syscall
+li $v0, 1
+syscall
+bgt $a0, 50, greaterThan 
+#lessThan: 
+la $a0, Win
+li $v0, 4 # "WIN!"
+syscall
+j loop
+
+greaterThan:
+la $a0, Lose
+li $v0, 4 # "LOSE!"
+syscall
+j loop
 
 li $v0, 10
 syscall
