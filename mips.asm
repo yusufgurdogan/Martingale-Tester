be064d65e36d8cf8ca25222b348316f9dec24659
Yusuf Gürdoğan <yusufgurdogan@protonmail.com>
2020-01-28 13:11:50 +0300

    Update mips.asm
    - Balance system works now.
    - Randomizer is fair: 0-49: WIN, 50-99: LOSE

--- mips.asm
+++ mips.asm
@@ -4,10 +4,13 @@ enterhouseedge: .asciiz "Enter house edge (%):"
 enterbetsize: .asciiz "Enter initial bet size:"
 entergoal: .asciiz "Enter your goal (for doubling your money, enter 2):"
 BetSizeGreater: .asciiz "Bet size can not be greater than your starting balance!\n"
-Win: .asciiz "WIN!\n"
-Lose: .asciiz "LOSE!\n"
+YouHaveLost: .asciiz "You have lost the game."
+YouHaveWon: .asciiz "You have won the game."
+Win: .asciiz "WIN!"
+Lose: .asciiz "LOSE!"
 betsize: .asciiz "bet size: "
-balance: .asciiz "balance: "
+balance: .asciiz " balance: "
+newLine: .asciiz "\n"
 .text
 .globl main
 main:
@@ -18,7 +21,7 @@ syscall
 
 li $v0, 5 # scanf balance
 syscall 
-move $a0, $v0
+move $t3, $v0
 
 la $a0, enterhouseedge
 li $v0, 4 # "Enter house edge (%):"
@@ -34,7 +37,8 @@ syscall
 
 li $v0, 5 # scanf bet size
 syscall 
-move $a2, $v0
+move $t4, $v0
+
 
 bgt $a2, $a0, IfBetSizeGreater # If bet size is greater, jump to IfBetSizeGreater
 
@@ -43,7 +47,7 @@ li $v0, 4 # "Enter your goal (for doubling your money, enter 2):"
 syscall
 
 li $v0, 5 # scanf goal
-syscall 
+syscall
 move $a3, $v0
 
 j loop
@@ -53,10 +57,21 @@ li $v0, 42 # generate random number
 syscall
 li $v0, 1
 syscall
-bgt $a0, 50, greaterThan 
-#lessThan: 
+bgt $a0, 49, greaterThan
+#lessThan:
 la $a0, Win
 li $v0, 4 # "WIN!"
+syscall
+
+la $a0, balance
+li $v0, 4 # balance:
+syscall
+add $t3, $t3, $t4 # add winned amount to the balance
+move $a0, $t3
+li $v0, 1
+syscall
+la $a0, newLine
+li $v0, 4 # "\n"
 syscall
 j loop
 
@@ -64,6 +79,18 @@ greaterThan:
 la $a0, Lose
 li $v0, 4 # "LOSE!"
 syscall
+
+la $a0, balance
+li $v0, 4 # balance:
+syscall
+sub $t3, $t3, $t4 # substract winned amount to the balance
+move $a0, $t3
+li $v0, 1
+syscall
+la $a0, newLine
+li $v0, 4 # "\n"
+syscall
+bge $t4, $t3, InsufficientBalance # If balance is insufficient, stop.
 j loop
 
 li $v0, 10
@@ -71,6 +98,13 @@ syscall
 
 IfBetSizeGreater:
 la $a0, BetSizeGreater
+li $v0, 4
+syscall
+li $v0, 10
+syscall
+
+InsufficientBalance:
+la $a0, YouHaveLost
 li $v0, 4
 syscall
 li $v0, 10
